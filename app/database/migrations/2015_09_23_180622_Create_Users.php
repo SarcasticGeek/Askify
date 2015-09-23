e2e3b3ca21f9f80f6d837c7eadb3db645ee44057
Khaled M.Fathy <eng.khaled.93@hotmail.com>
2015-09-23 21:25:49 +0200

    Issue #1, Create Database

---
+++ app/database/migrations/2015_09_23_180622_Create_Users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users',function($variable){
+			$variable->increments('id');
+			$variable->string('email')->unique();
+			$variable->string('username')->unique();
+			$variable->string('password');
+			$variable->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
