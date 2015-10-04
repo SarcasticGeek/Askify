af8dcb2f77ddcdfcacefe7640984548ab5783029
tahagoal <tahagoal@gmail.com>
2015-10-04 20:26:37 +0200

    nav bar

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -23,23 +23,19 @@
 		<!-- header section -->
   <div class="container">
       <div class="row">
-          	<nav class="navbar navbar-default">
+          	<nav class="navbar navbar-inverse">
           <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">Askify</a>
+              <a class="navbar-brand" href="#">
+                  <img alt="Brand" src="images/logo2.png" width="31" height="35" id="logo">
+              </a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav">
-                <li class="active">{{ HTML::link('/home', 'Askify') }}</li>
+                <li class="active">{{ HTML::link('/home', 'Home') }}</li>
                 <li>{{ HTML::link('/your_questions', 'Your Questions') }}</li>
                 <li>{{ HTML::link('/your_answers', 'Your Answers') }}</li>
 
@@ -73,7 +69,7 @@
           </div><!-- /.container-fluid -->
     </nav>
 
-        </div>
+  </div>
       @yield('content')
 
 
