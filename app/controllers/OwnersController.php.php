1a915b0bc31ee4af283c18d960831c4a58de9318
SarcasticGeek <m_essamfathalla@yahoo.com>
2015-10-09 22:03:21 +0200

    Add owner controller

--- app/controllers/OwnersController.php.php
+++ app/controllers/OwnersController.php.php
@@ -3,6 +3,15 @@
 class OwnersController extends BaseController {
 	public $restful = true;
 
+	public function show_my_tags(){
+		return View::make('owner.tags')
+			->with('title','My Tags')
+			->with('tags',Auth::User()->tags);
+	}
+	public function get_view_tag($id = null){
+		return View::make('owner.tag')->with('title','View Tag')->with('tag',Tag::find($id));
+	}
+
 }
 
 
