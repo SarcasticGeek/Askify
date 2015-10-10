efd0d1cbfe85c7c816c89fd663fafc51d58c32a2
Hatem93 <hatem.hussin1993@gmail.com>
2015-10-11 02:41:36 +0800

    Issue #13, configure logout

--- app/controllers/LogoutController.php
+++ app/controllers/LogoutController.php
@@ -5,6 +5,12 @@ class LogoutController extends BaseController{
 	
 
 	public function doLogout(){
+        if(Auth ::user()->confirmationcode == 'f')
+        {
+            $hauth=new Hybrid_Auth(app_path().'\config\fb_auth.php');
+            $hauth->logoutAllProviders();
+        }
+        Session::flush();
 		Auth::logout();
 		return Redirect::to('/');
 		
