59e68c0561f6af759f4ead70d1b64b2e5e4802b0
Hatem93 <hatem.hussin1993@gmail.com>
2015-10-11 00:36:55 +0800

    Issue #13, adding facebook login in login view /bug fix

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -79,7 +79,7 @@
 
       <div class="container-fluid">
           <a href="facebookauth">
-  	  	  	<button type="button" class="btnnn">Sign In Using facebook</button>
+              <button type="button" class="btnnn">Sign In Using facebook</button></a>>
   	<a href="login">
   	  	  	<button type="button" class="btnnn">Sign In</button>
   	</a>
