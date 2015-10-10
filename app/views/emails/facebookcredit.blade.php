7ab0ffe227a345fec742bf64e2404aa8c1c52ac5
Hatem93 <hatem.hussin1993@gmail.com>
2015-10-11 00:57:44 +0800

    Issue #13, facebook email user credientials view

---
+++ app/views/emails/facebookcredit.blade.php
@@ -0,0 +1,15 @@
+<div class="registermsg">
+<h1>Welcome To Askify</h1>
+<h2>Hi,{{$name}} !</h2>
+<p>Thanks For Siging in askify using facebook you login credientials are:</p>
+<p>username = {{$name}} .</p>
+    <p>password = {{$password}} .</p>
+<p>click to activate.</p>
+</div>
+<div class="panel-footer">
+    <div class="panel-body">
+                © 2015 Askify
+            </div>
+  </div>
+
+	</body>
