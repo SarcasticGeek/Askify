f938de90a6d5276a537c3c4e47310f943b3b09a0
SarcasticGeek <m_essamfathalla@yahoo.com>
2015-10-12 01:49:41 +0200

    Addin Sharing Buttons to A Question

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -11,7 +11,7 @@
 <link rel="stylesheet" type="text/css"href="{{ asset('css/bootstrap.min.css')}}">
 <link rel="stylesheet" type="text/css"href=" {{ asset('css/bootstrap.css')}}">
 <link rel="stylesheet" type="text/css"href="{{ asset('css/main.css') }}">
-
+<link rel="stylesheet" type="text/css"href="{{ asset('css/social-sharing.css') }}">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script type="text/javascript" src=" {{ asset('js/jquery-2.1.4.min.js')}}"></script>
