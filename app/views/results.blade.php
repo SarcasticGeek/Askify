d4f78bedff68566ac7cddb5189cbceef0b2755f7
Hatem93 <hatem.hussin1993@gmail.com>
2015-10-22 04:04:34 +0800

    fixes to navbar 2

--- app/views/results.blade.php
+++ app/views/results.blade.php
@@ -235,7 +235,7 @@ h1 {
 
 
     </nav>
-    <nav class="navbar navbar-default navbar-static-top" role="navigation"> 
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation"> 
         <div class="navbar-header page-scroll">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
                     <span class="sr-only">Toggle navigation</span>
