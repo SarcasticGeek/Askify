ace5dcffc9dca8a5ecdfa822cb928d2964856b06
SarcasticGeek <m_essamfathalla@yahoo.com>
2015-10-24 21:36:31 +0200

    add api for mobile

--- app/routes.php
+++ app/routes.php
@@ -105,9 +105,6 @@ Route::post('owner/tag/new',array('before' => 'auth','before'=>'csrf','before'=>
 //Login by google
 Route::get('auth/ViaGoogle/{auth?}',array('as'=>'authViaGoogle','uses'=>'AuthController@loginWithGoogle'));
 
-Route::get('api/questionlist/all',function(){
-    foreach ($variable as $key => $value) {
-        # code...
-    }
-    return Response::json(DB::table('questions')->get());
-});
+
+Route::get('api/questionlist/all','ApiController@getAllQs');
+Route::get('/api/questionlist/view/{user_id?}','ApiController@getQsByUserId');
