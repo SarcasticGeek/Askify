8cf51815db514b6f75d06662d2d461b5b1287a2f
Khaled Mohamed Fathy <eng.khaled.93@hotmail.com>
2015-11-01 16:35:47 +0200

    Add Footer

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -436,8 +436,8 @@
 			background-color: black;
 			color: white;
 			width: 100%;
-			position: absolute;
-			bottom: 0px;
+			position: relative;
+			bottom: -60px;
 		}
 		
 		/*responsive issues*/
@@ -685,6 +685,39 @@
 			</form>
 			</div>	
 	</div>
+
+
+	<footer class="section section-primary" >
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <p>© Copyright <?php echo date('Y');?></p>
+          </div>
+          <div class="col-sm-6">
+            <p class="text-info text-right">
+              <br>
+              <br>
+            </p>
+            <div class="row">
+              <div class="col-md-12 hidden-lg hidden-md hidden-sm text-left">
+                <a href="#"><i class="fa fa-3x fa-fw fa-instagram text-inverse"></i></a>
+                <a href="#"><i class="fa fa-3x fa-fw fa-twitter text-inverse"></i></a>
+                <a href="#"><i class="fa fa-3x fa-fw fa-facebook text-inverse"></i></a>
+                <a href="#"><i class="fa fa-3x fa-fw fa-github text-inverse"></i></a>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12 hidden-xs text-right">
+                <a href="#"><i class="fa fa-3x fa-fw fa-instagram text-inverse"></i></a>
+                <a href="#"><i class="fa fa-3x fa-fw fa-twitter text-inverse"></i></a>
+                <a href="#"><i class="fa fa-3x fa-fw fa-facebook text-inverse"></i></a>
+                <a href="#"><i class="fa fa-3x fa-fw fa-github text-inverse"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
 </div>
 
 
