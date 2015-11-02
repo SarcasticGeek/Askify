bb769589df648a3ba4c1685700aa969042d27f84
Khaled Mohamed Fathy <eng.khaled.93@hotmail.com>
2015-11-02 19:45:21 +0200

    Fix Bugs After Merge Medo

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -343,25 +343,6 @@ session_start();
 						@endforeach
 					</div>
 
-						{{-- Tags tab --}}
-				    <div role="tabpanel" class="tab-pane" id="tags">
-				    	<div class="questionlistajax">
-						</div>
-						<div class="questionlistajax1">
-						</div>
-						<div class="questionlistajax2">
-						</div>
-						<div class="questionlistajax3">
-						</div>
-						<div class="questionlistajax4">
-						</div>
-						<div class="questionlistajax5">
-						</div>
-						<div class="questionlistajax6">
-						</div>
-						<div class="questionlistajax7">
-						</div>
-						<div class="questionlistajax8">
 						{{$solved_questions->links()}}
 						</div>
 					
